348a7acb5b982d4bac43304861ebeca20bfc9815
Aaron Liew <aaron_liew1818@hotmail.com>
2022-01-02 16:39:25 +0800

    [COMPLETE] Complete basic auth

--- src/main/java/com/maybank/assessment/MaybankApplication.java
+++ src/main/java/com/maybank/assessment/MaybankApplication.java
@@ -5,11 +5,9 @@ import com.maybank.assessment.service.TransactionService;
 import com.maybank.assessment.service.TransactionServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class MaybankApplication {
 
     public static void main(String[] args) {
