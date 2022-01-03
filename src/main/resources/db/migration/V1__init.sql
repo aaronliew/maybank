1754ff91b783ccd44d4b3bf654fdf5cef293eacd
Aaron Liew <aaron_liew1818@hotmail.com>
2022-01-03 12:24:57 +0800

    [REFACTOR] Change date to datetime in sql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -6,8 +6,8 @@ CREATE TABLE `transaction` (
    `amount` bigint(20) DEFAULT NULL,
    `customer_id` varchar(255) DEFAULT NULL,
    `description` varchar(255) DEFAULT NULL,
-   `record_create_date` date DEFAULT NULL,
-   `record_update_date` date DEFAULT NULL,
+   `record_create_date` datetime DEFAULT NULL,
+   `record_update_date` datetime DEFAULT NULL,
    `transaction_date` datetime(6) DEFAULT NULL,
    PRIMARY KEY (`transaction_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
